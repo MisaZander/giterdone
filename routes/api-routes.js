9782bc55c92dfcad48867e25bd26547063d576f6
Alexander Osborn <misterzander@gmail.com>
2018-11-30 12:39:32 -0600

    Initial skeleton

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,15 +1,13 @@
-var db = require("../models");
-
 module.exports = function(app) {
-    app.get("/", function(req, res, next) {
-        if(req.session.views) {
-            req.session.views++;
-            //res.send("Page viewed " + req.session.views + " times.");
-            res.render("index", {views: req.session.views});
-        } else {
-            req.session.views = 1;
-            //res.send("Welcome aboard");
-            res.render("index", {views: req.session.views});
-        }
-    });
+    // app.get("/", function(req, res, next) {
+    //     if(req.session.views) {
+    //         req.session.views++;
+    //         //res.send("Page viewed " + req.session.views + " times.");
+    //         res.render("index", {views: req.session.views});
+    //     } else {
+    //         req.session.views = 1;
+    //         //res.send("Welcome aboard");
+    //         res.render("index", {views: req.session.views});
+    //     }
+    // });
 }
