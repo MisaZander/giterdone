89cc4a80c01e6c63e21a3f88abbb041335fccda3
Alexander Osborn <misterzander@gmail.com>
2018-12-05 10:36:58 -0600

    Obtain the DB userId

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,4 +1,5 @@
 var db = require("../models");
+var path = require("path");
 
 module.exports = function(app) {
   // // Load index page
@@ -24,4 +25,52 @@ module.exports = function(app) {
   // app.get("*", function(req, res) {
   //   res.render("404");
   // });
+
+  app.get(
+    "/",
+    function(req, res, next) {
+      //console.log(req.session.id);
+      // db.Session.findOne({
+      //   where: {
+      //     sid: req.sessionID
+      //   }
+      // })
+      //   .then(function(dbData) {
+      //     console.log(req.sessionID);
+      //     if () {
+      //       console.log("User ID: " + dbData.userId);
+      //     }
+      //     res.sendFile(
+      //       path.join(__dirname, "..", "public", "html", "index.html")
+      //     );
+      //   })
+      //   .catch(function(err) {
+      //     if (err) {
+      //       throw err;
+      //     }
+      //     //console.log(err);
+      //     res.end("Oops");
+      //   });
+
+      db.Session.findOne({
+        where: {
+          sid: req.sessionID
+        }
+      }).then(function(dbData) {
+        if (dbData === null || dbData === undefined) {
+          console.log("No session stored. Next!");
+          next();
+        } else {
+          console.log("You session userID is: " + dbData.userId);
+          res.sendFile(
+            path.join(__dirname, "..", "public", "html", "index.html")
+          );
+        }
+      });
+    },
+    function(req, res) {
+      //First visit
+      res.sendFile(path.join(__dirname, "..", "public", "html", "index.html"));
+    }
+  );
 };
