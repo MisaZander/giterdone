8cb0fff635ed5856eb9133cd00753f3598c2fb92
Alexander Osborn <misterzander@gmail.com>
2018-12-07 11:52:26 -0600

    Address issue #12: development and test environments now read the database connection information from a .env file in the project root folder

---
+++ config/config.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    logging: true
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    logging: false
+  },
+  production: {
+    useEnvVariable: process.env.JAWSDB_URL,
+    dialect: "mysql"
+  }
+};
